3aadc9c30d561c461b83a932b0cfc62d3d7a2464
12345ljx <72619485+12345ljx@users.noreply.github.com>
2022-05-15 14:16:27 +0800

    Update device.h

--- device/device.h
+++ device/device.h
@@ -41,7 +41,7 @@ bool release(long pid, int device);
 // 磁盘初始化
 void disk_init();
 
-// 申请一个数据块,返回块号;如果返回-1说明无空闲数据块
+// 申请一个数据块,返回块号;如果返回0说明无空闲数据块
 int new_block();
 // 输入块号,释放该物理块
 void free_block(int blockSeq);
@@ -50,7 +50,7 @@ bool block_write(int blockSeq, int offset, int charNum, char* buf);
 //读出物理块，形参列表:块号,开始读的位置在块中的偏移量,要读入的字节数,缓冲区指针;返回true代表操作成功
 bool block_read(int blockSeq, int offset, int charNum, char* buf);
 
-// 申请一个iNode,返回iNode指针;指针中的i_num是iNode编号;如果i_num为-1说明无空闲iNode块
+// 申请一个iNode,返回iNode指针;指针中的i_num是iNode编号;如果返回空指针说明无空闲iNode块
 iNode* new_iNode();
 // 释放一个iNode,根据参数中的inode->i_num释放磁盘中的iNode节点,并释放参数中的iNode指针
 void free_iNode(iNode* inode);
